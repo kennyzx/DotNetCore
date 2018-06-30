ae89b192ca0b0fc8e3e55f7b2fcdb5bae859a0e7
kennyzx <ikennyzx@gmail.com>
2018-06-30 13:38:53 +0800

    First commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(Environment.OSVersion);
+            Console.WriteLine(Environment.CurrentDirectory);
+            Console.WriteLine("Open a file...");
+            Console.ForegroundColor = ConsoleColor.White;
+            if (File.Exists("/etc/apache2/apache2.conf"))
+            {
+                foreach (var line in File.ReadAllLines("/etc/apache2/apache2.conf"))
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
