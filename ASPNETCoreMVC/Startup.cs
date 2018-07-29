864a5b403a6ad3d3eabf8795e3260721fdf9d984
kennyzx <ikennyzx@gmail.com>
2018-07-29 17:21:56 +0800

    Use EF/MySQL, configure error.

--- ASPNETCoreMVC/Startup.cs
+++ ASPNETCoreMVC/Startup.cs
@@ -22,6 +22,7 @@ namespace ASPNETCoreMVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddEntityFrameworkMySql().AddDbContext();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
