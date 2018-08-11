8ed2871a4c7b0d309d996574f9b103dad49c0766
kennyzx <ikennyzx@gmail.com>
2018-08-11 15:20:59 +0800

    Register DbContext as a service

--- ASPNETCoreMVC/Startup.cs
+++ ASPNETCoreMVC/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -22,6 +23,9 @@ namespace ASPNETCoreMVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<ubuntudbContext>((options)=> {
+                options.UseMySql(Configuration.GetConnectionString("UbuntuDatabase"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
