b05125dc29e6f300ff51e8683f86a5ae40c6573f
kennyzx <ikennyzx@gmail.com>
2018-11-03 14:51:12 +0800

    Create a new console app using 'dotnet new console' command

---
+++ UseSerilog/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UseSerilog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
