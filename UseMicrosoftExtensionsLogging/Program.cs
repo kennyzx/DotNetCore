71948f3d58ddb3b4e5b25286559264967e80e5bf
kennyzx <ikennyzx@gmail.com>
2019-08-10 23:23:10 +0800

    Use Microsoft.Extensions.Logging.Console

--- UseMicrosoftExtensionsLogging/Program.cs
+++ UseMicrosoftExtensionsLogging/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Newtonsoft.Json;
+using Microsoft.Extensions.Logging;
+
 
 namespace UseMicrosoftExtensionsLogging
 {
@@ -7,8 +9,14 @@ namespace UseMicrosoftExtensionsLogging
     {
         static void Main(string[] args)
         {
+            var factory = new LoggerFactory()
+                    .AddConsole(LogLevel.Debug);
             PersonModel person = new PersonModel { Name = "abc", Age = 123};
+            ILogger logger = factory.CreateLogger<Program>();
+
+            logger.Log(LogLevel.Debug, "step 1");
             Console.WriteLine($"Hello {JsonConvert.SerializeObject(person)}!");
+            logger.Log(LogLevel.Debug, "step 2");
         }
     }
 }
